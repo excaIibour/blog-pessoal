376a257a79e5557944fbfe386e4c981142e28973
Joseane Vieira de Oliveira <jojovieiradeoliveira@gmail.com>
2022-12-15 23:47:46 -0300

    commit
    commit

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
-    path: "/cv-web",
+    path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
